483cb351ccfa1a9efe46ba231e4a125996a1facd
wizawu <wizawu@gmail.com>
2017-04-15 04:24:21 +0800

    Fix test

--- src/js/test/test.tsx
+++ src/js/test/test.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 
-import { Nav } from "../../src/index"
+import { Nav } from "../index"
 
 function block(width: number, height: number, background: string) {
     return <div style={{ width, height, background }} />
