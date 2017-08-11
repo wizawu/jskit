f03b436f5a68a505b4e0eadd4c1283a62e827cf0
wizawu <wizawu@gmail.com>
2017-08-11 17:58:47 +0800

    Support more convertors

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 #!/usr/bin/env node
 
+if (process.argv[2] === undefined) {
+    console.error("Usage: cat some.json | object-path-cli <path> [convertor]")
+    console.error("Example:")
+    console.error("    cat package.json | object-path-cli version")
+    console.error("    cat package.json | object-path-cli version parseInt")
+    console.error("    cat package.json | object-path-cli bin toUpperCase")
+    console.error("    cat package.json | object-path-cli bin 'substr(2)'")
+    process.exit(1)
+}
+
 var objectPath = require("object-path")
 
 var buffer = ""
@@ -16,7 +26,13 @@ process.stdin.on("end", function () {
     var value = objectPath.get(json, path)
 
     if (conv) {
-        eval("console.log(" + conv + "(value))")
+        if (typeof global[conv] === "function") {
+            eval("console.log(" + conv + "(value))")
+        } else if (conv in value.__proto__) {
+            eval("console.log(value." + conv + "())")
+        } else {
+            eval("console.log(value." + conv + ")")
+        }
     } else {
         console.log(value)
     }
