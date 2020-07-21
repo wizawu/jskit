a18e65fb1f35399e902b059237f556da68f86e64
wizawu <wizawu@gmail.com>
2020-07-21 11:42:08 +0800

    Fix YAML comments and blank lines

--- fmt2/dist/format.js
+++ fmt2/dist/format.js
@@ -6,32 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.formatYAML = exports.formatXML = exports.formatJSON = void 0;
 var xml_js_1 = __importDefault(require("xml-js"));
 var yaml_1 = __importDefault(require("yaml"));
-var util_1 = require("util");
 yaml_1.default.scalarOptions.str.fold.lineWidth = 120;
 function formatJSON(input) {
-    var json = JSON.parse(input);
-    return JSON.stringify(json, null, 2) + "\n";
+    var doc = JSON.parse(input);
+    return JSON.stringify(doc, null, 2) + "\n";
 }
 exports.formatJSON = formatJSON;
 function formatXML(input) {
-    var xml = xml_js_1.default.xml2js(input);
-    return xml_js_1.default.js2xml(xml, { spaces: 2 }) + "\n";
+    var doc = xml_js_1.default.xml2js(input);
+    return xml_js_1.default.js2xml(doc, { spaces: 2 }) + "\n";
 }
 exports.formatXML = formatXML;
 function formatYAML(input) {
-    var opts = { schema: "yaml-1.1" };
-    var yaml = yaml_1.default.parse(input, opts);
-    if (util_1.isArray(yaml)) {
-        return yaml.map(function (obj) { return yaml_1.default.stringify([obj], opts); }).join("\n");
-    }
-    else if (util_1.isObject(yaml)) {
-        return Object.keys(yaml).map(function (key) {
-            var _a;
-            return yaml_1.default.stringify((_a = {}, _a[key] = yaml[key], _a), opts);
-        }).join("\n");
-    }
-    else {
-        return yaml_1.default.stringify(yaml, opts);
-    }
+    var doc = yaml_1.default.parseDocument(input, { indent: 2, schema: "yaml-1.1" });
+    return doc.toString();
 }
 exports.formatYAML = formatYAML;
--- fmt2/src/format.ts
+++ fmt2/src/format.ts
@@ -1,27 +1,19 @@
 import XML from "xml-js"
 import YAML from "yaml"
-import { isArray, isObject } from "util"
 
 YAML.scalarOptions.str.fold.lineWidth = 120
 
 export function formatJSON(input: string) {
-    let json = JSON.parse(input)
-    return JSON.stringify(json, null, 2) + "\n"
+    let doc = JSON.parse(input)
+    return JSON.stringify(doc, null, 2) + "\n"
 }
 
 export function formatXML(input: string) {
-    let xml = XML.xml2js(input)
-    return XML.js2xml(xml, { spaces: 2 }) + "\n"
+    let doc = XML.xml2js(input)
+    return XML.js2xml(doc, { spaces: 2 }) + "\n"
 }
 
 export function formatYAML(input: string) {
-    let opts: YAML.Options = { schema: "yaml-1.1" }
-    let yaml = YAML.parse(input, opts)
-    if (isArray(yaml)) {
-        return yaml.map(obj => YAML.stringify([obj], opts)).join("\n")
-    } else if (isObject(yaml)) {
-        return Object.keys(yaml).map(key => YAML.stringify({ [key]: yaml[key] }, opts)).join("\n")
-    } else {
-        return YAML.stringify(yaml, opts)
-    }
+    let doc = YAML.parseDocument(input, { indent: 2, schema: "yaml-1.1" })
+    return doc.toString()
 }
