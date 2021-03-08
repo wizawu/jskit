177b26dd558a7d0058dd7c27f1638ebc173fe33b
wizawu <wizawu@gmail.com>
2021-03-08 12:35:30 +0800

    Sort object recursively

--- fmt2i/src/format.ts
+++ fmt2i/src/format.ts
@@ -1,24 +1,32 @@
 import XML from "xml-js"
 import YAML from "yaml"
 
-export function formatJSON(input: string, sorted = false) {
-    let doc = JSON.parse(input)
-    if (sorted) {
-        doc = Object.keys(doc).sort().reduce((res, k) => {
-            res[k] = doc[k]
-            return res
-        }, {} as any)
+function sortObject(doc: any): any {
+  if (typeof doc !== "object") return doc
+  if (Array.isArray(doc)) return doc.map(it => sortObject(it))
+  return Object.keys(doc).sort().reduce((res, k) => {
+    if (typeof doc[k] === "object") {
+      res[k] = sortObject(doc[k] as any)
+    } else {
+      res[k] = doc[k]
     }
-    return JSON.stringify(doc, null, 2) + "\n"
+    return res
+  }, {} as any)
+}
+
+export function formatJSON(input: string, sorted = false) {
+  let doc = JSON.parse(input)
+  if (sorted) doc = sortObject(doc)
+  return JSON.stringify(doc, null, 2) + "\n"
 }
 
 export function formatXML(input: string) {
-    let doc = XML.xml2js(input)
-    return XML.js2xml(doc, { spaces: 2 }) + "\n"
+  let doc = XML.xml2js(input)
+  return XML.js2xml(doc, { spaces: 2 }) + "\n"
 }
 
 export function formatYAML(input: string, lineWidth: number = 80) {
-    YAML.scalarOptions.str.fold.lineWidth = lineWidth
-    let doc = YAML.parseDocument(input, { indent: 2, schema: "yaml-1.1" })
-    return doc.toString()
+  YAML.scalarOptions.str.fold.lineWidth = lineWidth
+  let doc = YAML.parseDocument(input, { indent: 2, schema: "yaml-1.1" })
+  return doc.toString()
 }
--- fmt2i/src/main.ts
+++ fmt2i/src/main.ts
@@ -8,22 +8,22 @@ const opts = process.argv[2]
 const source = process.argv[3]
 const target = process.argv[process.argv[4] === "{}" ? 3 : 4]
 const version = (() => {
-    let packagePath = path.resolve(__dirname, "..", "package.json")
-    return JSON.parse(fs.readFileSync(packagePath, "utf-8")).version
+  let packagePath = path.resolve(__dirname, "..", "package.json")
+  return JSON.parse(fs.readFileSync(packagePath, "utf-8")).version
 })()
 
 if (opts === "-v") {
-    console.log(version)
-    process.exit(0)
+  console.log(version)
+  process.exit(0)
 }
 
 function help(code = 0) {
-    console.log(`Version: ${version}`)
-    console.log(`Usage:`)
-    console.log(`  fmt2i -h                            Print help message`)
-    console.log(`  fmt2i -v                            Print version`)
-    console.log(`  fmt2i [-j|-x|-y] <source> [target]  Format JSON/XML/YAML`)
-    process.exit(code)
+  console.log(`Version: ${version}`)
+  console.log(`Usage:`)
+  console.log(`  fmt2i -h                            Print help message`)
+  console.log(`  fmt2i -v                            Print version`)
+  console.log(`  fmt2i [-j|-x|-y] <source> [target]  Format JSON/XML/YAML`)
+  process.exit(code)
 }
 
 if (opts === "-h") help(0)
@@ -33,27 +33,27 @@ if (!source) help(3)
 const input = fs.readFileSync(source, "utf-8")
 let output: string = ""
 switch (opts) {
-    case "-j":
-        output = formatJSON(input, false)
-        break
-    case "-J":
-        output = formatJSON(input, true)
-        break
-    case "-x":
-        output = formatXML(input)
-        break
-    case "-y":
-        output = formatYAML(input, 80)
-        break
-    case "-Y":
-        output = formatYAML(input, 120)
-        break
-    default:
-        help(1)
+  case "-j":
+    output = formatJSON(input, false)
+    break
+  case "-J":
+    output = formatJSON(input, true)
+    break
+  case "-x":
+    output = formatXML(input)
+    break
+  case "-y":
+    output = formatYAML(input, 80)
+    break
+  case "-Y":
+    output = formatYAML(input, 120)
+    break
+  default:
+    help(1)
 }
 
 if (target) {
-    fs.writeFileSync(target, output)
+  fs.writeFileSync(target, output)
 } else {
-    process.stdout.write(output)
+  process.stdout.write(output)
 }
